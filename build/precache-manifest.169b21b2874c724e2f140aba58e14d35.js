adb8a59b352a86621e709a78872169f01a72c8c6
Dimovy <55894047+Dimovy@users.noreply.github.com>
2020-09-14 02:58:19 +0300

    build

---
+++ build/precache-manifest.169b21b2874c724e2f140aba58e14d35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a10ff5f76756f21d874c2df027607ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "095aa5e3e0c98fcc0ddb",
+    "url": "/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "095aa5e3e0c98fcc0ddb",
+    "url": "/static/js/2.9d7b957c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d7b957c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2c7700df6fbc1c6a734",
+    "url": "/static/js/main.905cb281.chunk.js"
+  },
+  {
+    "revision": "884831fb6a04f8c5f070",
+    "url": "/static/js/runtime-main.fcd09925.js"
+  }
+]);
